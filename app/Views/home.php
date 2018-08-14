0a115f873efe84548a614360603a9f41e5987a23
Paul Rock <paul@drteam.rocks>
2018-08-14 16:09:27 +0300

    refactoring of html

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,53 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Click To Call Tutorial</title>
-	<!-- We use Twitter Bootstrap as the default styling for our page-->
-	<link rel="stylesheet"
-		href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-	<link rel="stylesheet"
-		href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-	<!-- Include CSS for our third-party telephone input jQuery plugin-->
-	<link rel="stylesheet" href="/vendor/intl-phone/css/intlTelInput.css">
+    <title>Click To Call Tutorial</title>
+    <!-- We use Twitter Bootstrap as the default styling for our page-->
+    <link rel="stylesheet"
+          href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet"
+          href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+    <!-- Include CSS for our third-party telephone input jQuery plugin-->
+    <link rel="stylesheet" href="/vendor/intl-phone/css/intlTelInput.css">
 </head>
 <body>
-	<div class="container">
-		<h1>Click To Call</h1>
-		<p>
-			Click To Call converts your website's users into engaged customers by
-			creating an easy way for your customers to contact your sales and
-			support teams right on your website.
-		</p>
-		<p>Here's an example of how it's done!</p>
-		<hr>
+<div class="container">
+    <h1>Click To Call</h1>
+    <p>Based on solution from official <a href="https://www.twilio.com/docs/voice/tutorials/click-to-call-php-laravel">demo
+            application</a> by Twilio.</p>
+    <hr>
 
-		<!-- C2C contact form-->
-		<div class="row">
-			<div class="col-md-12">
-				<form id="contactForm" role="form">
-					<div class="form-group">
-						<h3>Call Sales</h3>
-						<p class="help-block">
-							Are you interested in impressing your friends and
-							confounding your enemies? Enter your phone number
-							below, and our team will contact you right away.
-						</p>
-					</div>
-                    <label>Your number</label>
-                    <div class="form-group">
-                       <input class="form-control" type="text" name="userPhone" id="userPhone"
-                              placeholder="(651) 555-7889">
-                    </div>
-					<button type="submit" class="btn btn-default">
-						Contact Sales
-					</button>
-				</form>
-			</div>
-		</div>
-	</div>
-	<!-- Include page dependencies -->
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script src="/vendor/intl-phone/js/intlTelInput.min.js"></script>
-	<script src="/app.js"></script>
+    <!-- C2C contact form-->
+    <div class="row">
+        <div class="col-md-12">
+            <form id="contactForm" role="form">
+                <div class="form-group">
+                    <h3>Call to Client</h3>
+                    <ul class="help-block">
+                        <li>When call icon is clicked, first Twilio calls the phone number #1 (Admin)</li>
+                        <li>Once admin picked up the phone, the number #2 (provider) is called.</li>
+                        <li>Both connected.</li>
+                    </ul>
+                </div>
+                <label>Client's number</label>
+                <div class="form-group">
+                    <input class="form-control" type="text" name="userPhone" id="userPhone"
+                           placeholder="+1 201-555-5555">
+                </div>
+                <button type="submit" class="btn btn-default">
+                    Call to Client
+                </button>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- Include page dependencies -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="/vendor/intl-phone/js/intlTelInput.min.js"></script>
+<script src="/app.js"></script>
 </body>
 </html>
